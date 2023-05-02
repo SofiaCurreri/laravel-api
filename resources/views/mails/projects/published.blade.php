a88d66982dfabb384def05bd7788ae95e57e3e45
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-02 18:20:42 +0200

    laravel mailable markdown

--- resources/views/mails/projects/published.blade.php
+++ resources/views/mails/projects/published.blade.php
@@ -1,23 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<x-mail::message>
+# {{$published_text}}
+## {{$project->title}}
+ 
+<p>
+    {{$project->getAbstract(100)}}
+</p>
+ 
+@if ($project->is_published)   
+<x-mail::button :url="$button_url">
+  View Project
+</x-mail::button>
+@endif
+ 
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
 
-    <style>
-        body {
-            font-family: Arial. Arial, Helvetica, sans-serif;
-            text-align: center;
-            background-color: antiquewhite;
-        }
-    </style>
-</head>
-
-<body>
-    <h1>{{$published_text}}</h1>
-    <h2>{{$project->title}}</h2>
-
-    <p> {{$project->getAbstract(100)}}</p>
-</body>
-</html>
