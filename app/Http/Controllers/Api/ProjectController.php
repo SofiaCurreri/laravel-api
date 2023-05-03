a5d4dcef307b3cee6311989d6078560cd78baf23
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-03 17:17:31 +0200

    new function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
 
 class ProjectController extends Controller
 {
@@ -45,4 +46,21 @@ class ProjectController extends Controller
         return response()->json($project);
     }
 
+
+    public function getProjectsByType($type_id)
+    {
+        $projects = Project::where('type_id', $type_id)
+        ->where('is_published', true)
+        ->with('type', 'technologies')
+        ->orderBy('updated_at', 'DESC')
+        ->paginate(6);
+
+        $type = Type::find($type_id);
+
+        foreach($projects as $project) {
+            $project->image = $project->getImageUri();
+        }
+        
+        return response()->json(compact('projects', 'type'));
+    }
 }
