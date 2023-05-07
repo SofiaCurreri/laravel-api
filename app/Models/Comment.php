b33baab6ecef5faf871e9bd0ff8d2dbf2485a104
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-07 19:21:11 +0200

    fillable

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 {
     use HasFactory;
 
+    public $fillable = ['project_id', 'name', 'email', 'message'];
+
     public function project() {
         return $this->belongsTo(Project::class);
     }
